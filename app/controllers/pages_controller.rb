b3670df6800a18648928461f1a8d98693f17e2b6
Dany Badoinot <danyb@perfscript.fr>
2013-09-23 12:58:29 +0200

    Removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,20 @@
 class PagesController < ApplicationController
-  def home
-  end
 
-  def contact
-  end
+	def home
+		@titre = "Accueil"
+	end
+
+	def contact
+		@titre = "Contact"
+	end
+
+	def about
+		@titre = "A Propos"
+	end
+
+	def help
+		@titre = "Aide"
+	end
+
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    #
+    # Define @base_title here.
+    #
+    @base_title = "Simple App du Tutoriel Ruby on Rails"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "devrait avoir le bon titre" do
+      get 'home'
+      response.should have_selector("title",
+                :content => @base_title + " | Accueil")
     end
   end
 
@@ -13,6 +26,35 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+     it "devrait avoir le bon titre" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => @base_title + " | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+     it "devrait avoir le bon titre" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => @base_title + " | A Propos")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+     it "devrait avoir le bon titre" do
+      get 'help'
+      response.should have_selector("title",
+                        :content => @base_title + " | Aide")
     end
   end
 
