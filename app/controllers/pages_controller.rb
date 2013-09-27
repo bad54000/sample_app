a085e359a1ef7b45331a5291521c66dbe288b410
Dany Badoinot <danyb@perfscript.fr>
2013-09-27 13:30:02 +0200

    Added user microposts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
 
 	def home
 		@titre = "Accueil"
+		if signed_in?
+			@micropost = Micropost.new if signed_in?
+			@feed_items = current_user.feed.paginate(:page => params[:page])
+		end
 	end
 
 	def contact
